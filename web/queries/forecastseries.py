0241db3a9b7467887e235e8a82b3be6d306da2ac
Nicolas Schmid <nicolas@breiten.ch>
2026-01-22 16:59:50 +0100

    fix: bug in eventcount forecastseries

--- web/queries/forecastseries.py
+++ web/queries/forecastseries.py
@@ -1,18 +1,21 @@
 # for one forecastseries and modelconfig, count events per
 # modelrun and realization inside a bounding box
 EVENT_COUNT_SERIES = """
-    SELECT mr.forecast_oid AS forecast_oid,
+    SELECT f.oid AS forecast_oid,
            res.realization_id,
            COUNT(*) AS event_count
-    FROM eventforecast s
-    JOIN modelrun mr ON s.modelrun_oid = mr.oid
-    JOIN forecast f ON mr.forecast_oid = f.oid
-    JOIN modelresult res ON s.modelresult_oid = res.oid
-    WHERE mr.modelconfig_oid = :modelconfig_oid
-      AND f.forecastseries_oid = :forecastseries_oid
-      AND f.status = 'COMPLETED'
-      AND ST_Within(
-              s.coordinates,
-              ST_MakeEnvelope(:min_lon, :min_lat, :max_lon, :max_lat, 4326))
-    GROUP BY mr.forecast_oid, res.realization_id;
+    FROM forecast f
+    JOIN modelrun mr
+        ON f.oid = mr.forecast_oid
+        AND mr.modelconfig_oid = :modelconfig_oid
+    JOIN modelresult res
+        ON mr.oid = res.modelrun_oid
+    JOIN eventforecast s
+        ON res.oid = s.modelresult_oid
+        AND ST_Within(
+            s.coordinates,
+            ST_MakeEnvelope(:min_lon, :min_lat, :max_lon, :max_lat, 4326))
+    WHERE f.forecastseries_oid = :forecastseries_oid
+        AND f.status = 'COMPLETED'
+    GROUP BY f.oid, res.realization_id;
 """
